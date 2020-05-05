febfd93a67284279e68d7bd3dd10464038fd9105
akhomuto <andrey.khomutov@t-systems.com>
2020-05-05 16:03:49 +0300

    add CommandLineRunner for prepare data in DB before start

---
+++ src/main/java/com/learnspringreactive/initialize/ItemDataInitializer.java
@@ -0,0 +1,41 @@
+package com.learnspringreactive.initialize;
+
+import com.learnspringreactive.document.Item;
+import com.learnspringreactive.repository.ItemReactiveRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class ItemDataInitializer implements CommandLineRunner {
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        initialDataSetUp();
+    }
+
+    public List<Item> data() {
+        return Arrays.asList(
+                new Item(null, "Samsung TV", 399.99),
+                new Item(null, "LG TV", 799.99),
+                new Item(null, "Sony TV", 199.99),
+                new Item(null, "Philips TV", 899.99)
+        );
+
+    }
+
+    private void initialDataSetUp() {
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(data()))
+                .flatMap(itemReactiveRepository::save)
+                .thenMany(itemReactiveRepository.findAll())
+                .subscribe(item -> System.out.println("Item inserted from CommandLineRunner: " + item)); //Do not never block, call subscribe
+    }
+}
